6fb55af255ac2b67f08b1c2531fda7529d59489a
Luke Plaster <notatestuser@gmail.com>
2014-12-09 23:05:05 +0100

    Added remaining tests

---
+++ test/test-line-parser.js
@@ -0,0 +1,53 @@
+/* global exports:true */
+/* global require:true */
+
+'use strict';
+
+var LogLineParserStream = require('../lib/log-line-parser');
+
+// mock LogStatsCollector
+var lastRecordedStats = null;
+var mockCollector = {
+    recordRequest: function(route, dyno, latency) {
+        lastRecordedStats = {
+            route: route,
+            dyno: dyno,
+            latency: latency
+        };
+    }
+};
+// a sample line from the logs
+var sampleLine = '2014-01-09T06:16:53.742892+00:00 heroku[router]: at=info method=GET path=/api/users/100002266342173/count_pending_messages host=services.pocketplaylab.com fwd="94.66.255.106" dyno=web.8 connect=9ms service=9ms status=304 bytes=0';
+
+// the LogLineParserStream instance under test
+var parser;
+
+exports.setUp = function(done) {
+    parser = new LogLineParserStream(mockCollector);
+    done();
+};
+
+exports.testParseLine = function(test) {
+    var parsed = parser.parseLine(sampleLine);
+    test.deepEqual(parsed, {
+        route: '/api/users/{user_id}/count_pending_messages',
+        path:  '/api/users/100002266342173/count_pending_messages',
+        dyno:  'web.8',
+        times: {
+            connect: 9,
+            service: 9
+        }
+    });
+    test.done();
+};
+
+exports.testWriteLine = function(test) {
+    // emulate writing a line to the stream
+    parser._write(sampleLine);
+    test.deepEqual(lastRecordedStats, {
+        route: '/api/users/{user_id}/count_pending_messages',
+        dyno:  'web.8',
+        latency: 18
+    });
+    test.done();
+};
---
+++ test/test-log-stats-collector.js
@@ -0,0 +1,48 @@
+/* global exports:true */
+/* global require:true */
+
+'use strict';
+
+var LogStatsCollector = require('../lib/log-stats-collector');
+
+// the LogLineParser instance under test
+var statsCollector;
+
+exports.setUp = function(done) {
+    statsCollector = new LogStatsCollector();
+    done();
+};
+
+exports.testRecordRequestAndGetKnownRoutes = function(test) {
+    statsCollector.recordRequest('/route/1', 'dyno.1', 10);
+    statsCollector.recordRequest('/route/2', 'dyno.2', 20);
+    statsCollector.recordRequest('/route/3', 'dyno.3', 30);
+    var knownRoutes = statsCollector.getKnownRoutes();
+    test.deepEqual(knownRoutes, [
+        '/route/1',
+        '/route/2',
+        '/route/3'
+    ]);
+    test.done();
+};
+
+exports.testRecordRequestAndGetStatsForSingleRoute = function(test) {
+    statsCollector.recordRequest('/route/1', 'dyno.1', 10);
+    statsCollector.recordRequest('/route/1', 'dyno.2', 20);
+    statsCollector.recordRequest('/route/1', 'dyno.1', 30);
+    statsCollector.recordRequest('/route/1', 'dyno.1', 20);
+    var stats = statsCollector.getStatsForRoute('/route/1');
+    test.deepEqual(stats, {
+        hits: 4,
+        dynoHits: {
+            'dyno.1': 3,
+            'dyno.2': 1
+        },
+        responseTimes: {
+            mean: 20,
+            mode: 20,
+            median: 20
+        }
+    });
+    test.done();
+};
